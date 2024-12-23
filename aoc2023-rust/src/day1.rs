d4a42eb20d58920b082f211d2c11bd0df1a2c3c0
Stefan Walter <stefan_walter@posteo.de>
2024-12-23 12:30:54 +0100

    aoc2023-rust day1 part1

---
+++ aoc2023-rust/src/day1.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+pub fn run() {
+    let content = fs::read_to_string("../../advent-of-code-data/aoc2023/Day1.txt").unwrap();
+
+    let mut sum = 0;
+
+    for line in content.lines() {
+        let first: u32 = line.chars().find_map(|x| x.to_digit(10)).unwrap();
+        let last: u32 = line.chars().rev().find_map(|x| x.to_digit(10)).unwrap();
+
+        sum += first * 10 + last;
+    }
+
+    println!("Day1: {}", sum);
+}
