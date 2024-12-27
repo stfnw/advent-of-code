89f637c6592a663277d6c8f1b21ff935ebf2f637
Stefan Walter <stefan_walter@posteo.de>
2024-12-27 08:24:21 +0100

    aoc2023-haskell day2 part1 reformat

--- aoc2023-haskell/src/Day2.hs
+++ aoc2023-haskell/src/Day2.hs
@@ -7,51 +7,51 @@ import Text.Read (readMaybe)
 
 run :: IO ()
 run = do
-  contents <- readFile "../../advent-of-code-data/aoc2023/Day2.txt"
-  putStrLn $ "Day2: " ++ show (run' contents)
+    contents <- readFile "../../advent-of-code-data/aoc2023/Day2.txt"
+    putStrLn $ "Day2: " ++ show (run' contents)
 
 data Game = Game {gameid :: Int, cubes :: [Cube]} deriving (Show)
 
 data Cube = Cube {red :: Int, green :: Int, blue :: Int} deriving (Show)
 
 instance Semigroup Cube where
-  Cube r1 g1 b1 <> Cube r2 g2 b2 = Cube (r1 + r2) (g1 + g2) (b1 + b2)
+    Cube r1 g1 b1 <> Cube r2 g2 b2 = Cube (r1 + r2) (g1 + g2) (b1 + b2)
 
 instance Monoid Cube where
-  mempty = Cube 0 0 0
+    mempty = Cube 0 0 0
 
 run' :: String -> Int
 run' s =
-  let games = traverse parseGame $ lines s
-      validGames = filter isValidGame <$> games
-      validIds = map gameid <$> validGames
-   in maybe (error "Invalid input") sum validIds
+    let games = traverse parseGame $ lines s
+        validGames = filter isValidGame <$> games
+        validIds = map gameid <$> validGames
+     in maybe (error "Invalid input") sum validIds
 
 -- e.g.: Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 parseGame :: String -> Maybe Game
 parseGame line = case splitOn ": " line of
-  [g, c] ->
-    let gameId = readMaybe . last $ words g
-     in Game <$> gameId <*> parseCubes c
-  _ -> Nothing
+    [g, c] ->
+        let gameId = readMaybe . last $ words g
+         in Game <$> gameId <*> parseCubes c
+    _ -> Nothing
 
 -- e.g.: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 parseCubes :: String -> Maybe [Cube]
 parseCubes s =
-  let cs = splitOn "; " s
-   in traverse parseCube cs
+    let cs = splitOn "; " s
+     in traverse parseCube cs
 
 -- e.g.: 3 blue, 4 red
 parseCube :: String -> Maybe Cube
 parseCube s =
-  let entries = splitOn ", " s
-   in mconcat <$> traverse parseEntry entries
+    let entries = splitOn ", " s
+     in mconcat <$> traverse parseEntry entries
 
 -- e.g.: 3 blue
 parseEntry :: String -> Maybe Cube
 parseEntry entry = case words entry of
-  [n, c] -> createCube c =<< readMaybe n
-  _ -> Nothing
+    [n, c] -> createCube c =<< readMaybe n
+    _ -> Nothing
 
 createCube :: String -> Int -> Maybe Cube
 createCube "red" n = Just $ Cube n 0 0
--- aoc2023-haskell/src/Main.hs
+++ aoc2023-haskell/src/Main.hs
@@ -5,5 +5,5 @@ import qualified Day2
 
 main :: IO ()
 main = do
-  Day1.run
-  Day2.run
+    Day1.run
+    Day2.run
