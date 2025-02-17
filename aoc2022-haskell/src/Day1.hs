210de28477abf54723eb2b9792bb7f83f7d17ba2
Stefan Walter <stefan_walter@posteo.de>
2025-02-17 05:56:55 +0100

    aoc2022-haskell day1 part2

--- aoc2022-haskell/src/Day1.hs
+++ aoc2022-haskell/src/Day1.hs
@@ -2,17 +2,29 @@
 
 module Day1 where
 
+import Data.List (sort)
+
 run :: IO ()
 run = do
     contents <- readFile "../../advent-of-code-data/aoc2022/Day1.txt"
-    putStrLn $ "Day1: " ++ show (run' contents)
+    putStrLn $ "Day1 part1: " ++ show (runPart1 contents)
+    putStrLn $ "Day1 part2: " ++ show (runPart2 contents)
 
-run' :: String -> Int
-run' contents =
-    maximum $
-        map
-            (sum . map (read :: String -> Int))
-            (foldl foldF [] $ lines contents)
+runPart1 :: String -> Int
+runPart1 = runTopElves 1
+
+runPart2 :: String -> Int
+runPart2 = runTopElves 3
+
+runTopElves :: Int -> String -> Int
+runTopElves ntop contents =
+    sum $
+        take ntop $
+            reverse $
+                sort $
+                    map
+                        (sum . map (read :: String -> Int))
+                        (foldl foldF [] $ lines contents)
   where
     foldF :: [[String]] -> String -> [[String]]
     foldF [] _ = [[]]
