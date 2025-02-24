d6e4f63dd613bd7d8863a4e8c386a665ea716cc6
Stefan Walter <stefan_walter@posteo.de>
2025-02-24 18:51:50 +0100

    aoc2022-haskell day4 part2

--- aoc2022-haskell/src/Day4.hs
+++ aoc2022-haskell/src/Day4.hs
@@ -5,13 +5,14 @@ module Day4 where
 run :: IO ()
 run = do
   contents <- readFile "../../advent-of-code-data/aoc2022/Day4.txt"
-  putStrLn $ "Day4 part1: " ++ runPart1 contents
+  putStrLn $ "Day4 part1: " ++ processInput isFullyOverlapping contents
+  putStrLn $ "Day4 part2: " ++ processInput isPartiallyOverlapping contents
 
-runPart1 :: String -> String
-runPart1 = show . length . filter id . map processLine . lines
+processInput :: ([[Int]] -> Bool) -> String -> String
+processInput f = show . length . filter id . map (f . pair) . lines
 
-processLine :: String -> Bool
-processLine = isFullyOverlapping . map (map read . words . replace '-' ' ') . words . replace ',' ' '
+pair :: String -> [[Int]]
+pair = map (map read . words . replace '-' ' ') . words . replace ',' ' '
 
 replace :: Char -> Char -> String -> String
 replace c d = map (\a -> if a == c then d else a)
@@ -21,3 +22,7 @@ isFullyOverlapping [[a1, a2], [b1, b2]] =
   (a1 <= b1 && b2 <= a2)
     || (b1 <= a1 && a2 <= b2)
 isFullyOverlapping _ = error "not supposed to happen"
+
+isPartiallyOverlapping :: [[Int]] -> Bool
+isPartiallyOverlapping [[a1, a2], [b1, b2]] = not (a2 < b1 || b2 < a1)
+isPartiallyOverlapping _ = error "not supposed to happen"
